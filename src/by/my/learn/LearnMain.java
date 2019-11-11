98083ad28234cec7f0e74f5f21c19f404a9a0a16
dimitrius-brest <d_der@mail.ru>
2019-11-11 17:19:00 +0300

    some experiments

--- src/by/my/learn/LearnMain.java
+++ src/by/my/learn/LearnMain.java
@@ -1,8 +1,12 @@
 package by.my.learn;
 
+import java.util.Arrays;
+
 public class LearnMain {
-    static Object obj[];
     public static void main(String[] args) {
-        System.out.println(obj);
+       int[] array = {21,22,23,24};
+       int[] hold = {49,48,47,46,45,44,43,42};
+       System.arraycopy(array, 0, hold, 2, array.length);
+       System.out.println(Arrays.toString(hold));
     }
 }
